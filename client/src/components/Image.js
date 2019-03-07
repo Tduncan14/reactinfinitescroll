5c2f7c63f1e96aa9756510a707d1dd60a39fa79f
unknown <nbatre1@yahoo.com>
2019-03-07 15:24:24 -0600

    keep moving forward

---
+++ client/src/components/Image.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function Image({image}){
+
+    return <img className="single-photo" src={image.urls.thumb}  alt="images"/>
+
+    
+}
+
+export default Image 
--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const app = express();
 
 app.get('/api/photos',(req,res) =>{
     
-    unsplash.photos.listPhotos(1,30)
+    unsplash.photos
+     .listPhotos(req.query.start,req.query.count)
      .then(toJson)
      .then(json => res.json(json));
 
