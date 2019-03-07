697ae36b69023f0807c3bc4938e8bb54fbe04224
unknown <nbatre1@yahoo.com>
2019-03-07 11:04:45 -0600

    keep moving forward

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports ={
+    APPLICATION_ID: process.env.APPLICATION_ID || '02031c73a44b22bfab6eecc1bea0d0f6923f042d5b41e85bfdcf23a10a31d01f',
+    SECRET: process.env.SECRET || 'c0b5e8e332caab6a484fbeff19bcd11ac1d55cd90c807153d0d73723422670d2'
+
+}
